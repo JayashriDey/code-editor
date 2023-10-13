689f0c9b8c9a46c69e9010ddfdf6824df16a3a1a
Jayashri Dey <123147911+JayashriDey@users.noreply.github.com>
2023-10-13 15:45:15 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,14 @@
+import Code from './src/Code';
+import Result from './Result';
+import Header from './src/Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <Code />
+            <Result />
+        </>
+    )
+}
+export default Home;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Home from './App';
+import DataProvider from './App';
+
+function App() {
+  return (
+    <DataProvider>    
+      <Home/>
+    </DataProvider>
+  );
+}
+export default App;
